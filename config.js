4ab0b4c9cd332ba4641b13010142bd59baafa083
Lars Kåre Skjørestad <lars.kaare@skjorestad.com>
2018-10-28 16:19:27 +0100

    Removing mongo from config

--- config.js
+++ config.js
@@ -75,14 +75,3 @@ exports.creds = {
 
 // The url you need to go to destroy the session with AAD
 exports.destroySessionUrl = 'https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=http://localhost:3000';
-
-// If you want to use the mongoDB session store for session middleware, set to true; otherwise we will use the default
-// session store provided by express-session.
-// Note that the default session store is designed for development purpose only.
-exports.useMongoDBSessionStore = false;
-
-// If you want to use mongoDB, provide the uri here for the database.
-exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
-
-// How long you want to keep session in mongoDB.
-exports.mongoDBSessionMaxAge = 24 * 60 * 60;  // 1 day (unit is second)
