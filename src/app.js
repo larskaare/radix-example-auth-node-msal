72f1737ae44d17b7ce7a11438f1f14132b711e90
Lars Kaare Skjorestad <lars.kaare@skjorestad.com>
2018-12-20 09:45:36 +0100

    Trigger build for w51

--- src/app.js
+++ src/app.js
@@ -225,7 +225,7 @@ app.use('/me', meRouter);
 
 app.get('/login',
     function(req, res, next) {
-        log.info('Login page requested');
+        // log.info('Login page requested');
         passport.authenticate('azuread-openidconnect', 
             { 
                 response: res,                      // required
